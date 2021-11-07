7b8f431667f2b5fc13cbca891deeb60626170f8d
Logan Willaumez <Logan.willaumez@gmail.com>
2021-11-07 11:00:27 +0100

    correct logic of JS + add error message design

---
+++ app.js
@@ -0,0 +1,18 @@
+const btn = document.querySelector('.notify');
+const form = document.querySelector('form');
+const input = document.querySelector('input')
+const regExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+btn.addEventListener("click",(e)=>{
+e.preventDefault();
+if (!input.value.match(regExp)){
+resetAnim(form)
+}
+else{
+    form.classList.remove('active')
+}
+})
+let resetAnim = (e) =>{
+    e.classList.remove('active')
+    void e.offsetWidth
+    e.classList.add('active')
+}
