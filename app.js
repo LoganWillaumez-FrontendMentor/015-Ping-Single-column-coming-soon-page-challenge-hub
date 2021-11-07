0590adade0efbfaf4957daf9376f5e3f711ab291
Logan Willaumez <Logan.willaumez@gmail.com>
2021-11-07 15:47:34 +0100

    add correct state

--- app.js
+++ app.js
@@ -7,12 +7,16 @@ btn.addEventListener("click", (e) => {
   e.preventDefault();
   if (!input.value.match(regExp)) {
     resetAnim(main);
+    main.classList.remove('ok')
   } else {
     main.classList.remove("active");
+    main.classList.add("ok");
+    document.querySelector('.error-message').textContent='Perfect ! ';
   }
 });
 let resetAnim = (e) => {
   e.classList.remove("active");
   void e.offsetWidth;
   e.classList.add("active");
+    document.querySelector('.error-message').textContent='Please provide a valid email address ! ';
 };
